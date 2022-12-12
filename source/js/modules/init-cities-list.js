ee72eac2fca98d6b753501e7bdea60a71c74bd91
StasyaShu <stanislava.semenova@gmail.com>
2022-12-12 10:23:27 +0300

    форматирование

--- source/js/modules/init-cities-list.js
+++ source/js/modules/init-cities-list.js
@@ -1,20 +1,20 @@
 const chooseCity = () => {
-  const cityTab = document.querySelector("#city");
-  const cityList = document.querySelector("#cities-list");
-  const saveButton = document.querySelector(".city-form__submit-button");
+  const cityTab = document.querySelector('#city');
+  const cityList = document.querySelector('#cities-list');
+  const saveButton = document.querySelector('.city-form__submit-button');
 
   const openMenu = () => {
-    cityTab.classList.add("is-chosen");
-    cityList.classList.add("city-list--show");
+    cityTab.classList.add('is-chosen');
+    cityList.classList.add('city-list--show');
   };
 
   const closeMenu = () => {
-    cityTab.classList.remove("is-chosen");
-    cityList.classList.remove("city-list--show");
+    cityTab.classList.remove('is-chosen');
+    cityList.classList.remove('city-list--show');
   }
 
   const documentClickHandler = (evt) => {
-    if (evt.target.classList.contains("city-form__item")) {
+    if (evt.target.classList.contains('city-form__item')) {
       return;
     } else {
       closeMenu();
@@ -22,35 +22,35 @@ const chooseCity = () => {
   }
 
   if (cityTab) {
-    cityTab.addEventListener("click", (evt) => {
+    cityTab.addEventListener('click', (evt) => {
       evt.preventDefault();
       openMenu();
-      const regionInput = cityList.querySelector("#region");
+      const regionInput = cityList.querySelector('#region');
       regionInput.focus();
     });
   }
 
   if (cityList) {
-    cityList.addEventListener("click", (evt) => {
+    cityList.addEventListener('click', (evt) => {
       const target = evt.target;
 
-      if (!target.classList.contains("city-form__item")) {
+      if (!target.classList.contains('city-form__item')) {
         return;
       } else {
-        target.classList.add("city-form__item--active");
+        target.classList.add('city-form__item--active');
 
-        const cityFormHeader = document.querySelector(".city-form__header");
-        const spanWithCityName = document.createElement("span");
+        const cityFormHeader = document.querySelector('.city-form__header');
+        const spanWithCityName = document.createElement('span');
         spanWithCityName.textContent = target.textContent;
         cityFormHeader.appendChild(spanWithCityName);
 
-        const city = document.querySelector(".city__header");
-        const cityTabText = city.querySelector("span");
+        const city = document.querySelector('.city__header');
+        const cityTabText = city.querySelector('span');
         cityTabText.textContent = target.textContent;
 
-        saveButton.classList.add("button--active");
+        saveButton.classList.add('button--active');
 
-        document.addEventListener("click", documentClickHandler);
+        document.addEventListener('click', documentClickHandler);
       }
     });
   }
@@ -58,4 +58,4 @@ const chooseCity = () => {
   document.removeEventListener('click', documentClickHandler);
 };
 
-export { chooseCity };
+export {chooseCity};
